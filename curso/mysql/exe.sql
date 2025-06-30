5db2b13b095c35d7d85f1697f198bbb813457c93
Celso Y. Ishida <celsoishida@gmail.com>
2025-06-30 17:01:15 -0300

    1a versão apostila Nodejs

--- curso/mysql/exe.sql
+++ curso/mysql/exe.sql
@@ -1,3 +1,6 @@
+/*
+saldomovimentacao  = 1 (admissão) ou -1 (demissão)
+*/
 CREATE TABLE Movimentacao (
     competenciamov INT,
     regiao INT,
@@ -59,3 +62,18 @@ SELECT sexo,
        max(salario) AS máximo
 FROM Movimentacao
 GROUP BY sexo;
+
+-- Exemplos de inserção na tabela Movimentacao
+INSERT INTO Movimentacao (
+    competenciamov, regiao, uf, municipio, secao, subclasse, saldomovimentacao,
+    cbo2002ocupacao, categoria, graudeinstrucao, idade, horascontratuais, racacor,
+    sexo, tipoempregador, tipoestabelecimento, tipomovimentacao, tipodedeficiencia,
+    indtrabintermitente, indtrabparcial, salario, tamestabjan, indicadoraprendiz,
+    origemdainformacao, competenciadec, indicadordeforadoprazo, unidadesalariocodigo,
+    valorsalariofixo
+) VALUES
+(202401, 1, 41, 410690, 'A', 1234, 1, 211220, 1, 6, 30, 44.00, 1, 1, 1, 1, 1, 0, 0, 0, 12000.00, 10, 0, 1, 202401, 0, 1, 12000.00),
+(202401, 1, 41, 410690, 'B', 5678, 1, 317105, 2, 7, 25, 40.00, 2, 2, 2, 2, 2, 0, 0, 0, 8000.00, 20, 1, 2, 202401, 0, 2, 8000.00),
+(202401, 1, 41, 410690, 'C', 9101, -1, 317110, 1, 8, 40, 44.00, 1, 1, 1, 1, 1, 0, 0, 0, 9500.00, 15, 0, 1, 202401, 0, 1, 9500.00),
+(202401, 1, 41, 410690, 'D', 1121, 1, 317120, 2, 9, 35, 36.00, 2, 2, 2, 2, 2, 0, 0, 0, 7000.00, 12, 1, 2, 202401, 0, 2, 7000.00);
+
